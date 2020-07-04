f88b0172813e1da883e198070bd07376d7e1dd14
nasimuddin1140@gmail.com <nasimuddin1140@gmail.com>
2020-07-05 00:43:57 +0600

    Install Vue router and Customize fronend

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
+
 require('./bootstrap');
+window.Vue = require('vue');
+// vue router
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+
+
+const app = new Vue({
+    el: '#app',
+});
